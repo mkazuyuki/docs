3014f4323746db80d07133f6e0c1b86f2e9d9600
Rei Iga <31835726+Igaigasuru@users.noreply.github.com>
2018-07-13 20:37:11 +0900

    Update MirrorOn.sh

--- utilities/MirrorOn.sh
+++ utilities/MirrorOn.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # ----------------------------------
-# Make NIC-A on ESXi-A and NIC-D on ESXi-B Link Down
+# Make NIC-A on ESXi-A and NIC-D on ESXi-B Link UP
 # ----------------------------------
 
 esxcfg-vswitch Mirror_vswitch -L vmnic_100101
