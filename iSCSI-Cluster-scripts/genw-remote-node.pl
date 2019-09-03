77ecfe082e5f88579ba6aceacf5d25047868a7a0
mkazuyuki <k-miyamoto@pu.jp.nec.com>
2019-09-03 16:14:44 +0900

    Bugfix

--- iSCSI-Cluster-scripts/genw-remote-node.pl
+++ iSCSI-Cluster-scripts/genw-remote-node.pl
@@ -90,7 +90,7 @@ sub IsRemoteClpOffline {
 	my $nodelocal	= "";
 	my $statremote	= "";
 
-	execution("clpstat");
+	&execution("clpstat");
 	foreach(@lines){
 		chomp;
 		if (/^\s{4}(\S+?)\s.*: (.+?)\s/) {
